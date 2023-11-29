f47f49d1820faeff1374c2f909299b999a892882
Kaue Ribeiro Costa <ribeirokaue70@gmail.com>
2023-11-29 10:36:39 -0400

    Alteração script

--- script_monitoramento.sh
+++ script_monitoramento.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
-
-# Defina as variáveis
-servico="httpd"  # Substitua "apache2" pelo nome real do seu serviço
+servico="httpd"  
 diretorio_nfs="/srv/nfs/kaue"
 data_hora=$(date '+%d-%m-%y %H:%M:%S')
 arquivo_online="$diretorio_nfs/servico_online.txt"
 arquivo_offline="$diretorio_nfs/servico_offline.txt"
 
-# Verifique a conectividade com o serviço (altere para o comando relevante para o seu caso)
 if systemctl is-active --quiet $servico; then
     status="ONLINE"
     mensagem="O serviço está online."
@@ -17,14 +13,10 @@ else
     mensagem="O serviço está offline."
     arquivo_saida=$arquivo_offline
 fi
-
-# Crie a mensagem formatada
 mensagem_formatada="$data_hora - $servico - Status: $status - $mensagem"
 
-# Imprima a mensagem no console
 echo $mensagem_formatada
 
-# Salve a mensagem no arquivo correspondente
 echo $mensagem_formatada >> $arquivo_saida
 
 
